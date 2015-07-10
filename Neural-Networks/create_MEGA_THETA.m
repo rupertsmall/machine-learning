fda2f688f566e0e9dea780404c14f29e8b61ef43
rupert small <rupertsmall@users.noreply.github.com>
2015-07-10 14:13:20 +0100

    Update create_MEGA_THETA.m

--- Neural-Networks/create_MEGA_THETA.m
+++ Neural-Networks/create_MEGA_THETA.m
@@ -5,8 +5,7 @@ function [MEGA_THETA] = create_MEGA_THETA(xi)
 L = length(xi);
 sum_xi = sum(xi);
 rows = sum_xi - xi(1);
-cols = sum_xi - xi(L) + L - 1;
-% put some ones in
-MEGA_THETA = eye(rows,cols);
+cols = sum_xi - xi(L) + L - 1;      %  include base unit for each layer
+MEGA_THETA = eye(rows,cols);        %  put some ones in
  
 end
